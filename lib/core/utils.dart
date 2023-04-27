77014f5a65ee977325ec761ad33291d68dd79e43
AB0s <93539709+AB0s@users.noreply.github.com>
2023-04-27 22:00:25 +0600

    additional pages

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
@@ -10,3 +11,8 @@ void showSnackBar(BuildContext context, String text) {
     );
 }
 
+Future<FilePickerResult?> pickImage() async {
+  final image = await FilePicker.platform.pickFiles(type: FileType.image);
+
+  return image;
+}
