367aff1549f11b16b61a7fc3c90d1a130228eb2e
Miras <91594617+laqube@users.noreply.github.com>
2023-06-14 00:40:26 +0600

    doc | community
    Added documentation to the /community folder files

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -18,6 +18,12 @@ final userCommunitiesProvider = StreamProvider((ref) {
   final communityController = ref.watch(communityControllerProvider.notifier);
   return communityController.getUserCommunities();
 });
+// A provider named `userCommunitiesProvider` that exposes a stream of user communities.
+// It uses the `StreamProvider` class to provide the stream of data.
+// The stream is obtained by calling the `getUserCommunities` method on the `communityControllerProvider` notifier.
+// The `communityControllerProvider` is accessed using the `ref.watch` function.
+// This provider is responsible for supplying the user communities data to the consumer widgets in the application.
+// Usage: `final userCommunities = useProvider(userCommunitiesProvider);`
 
 final communityControllerProvider = StateNotifierProvider<CommunityController, bool>((ref) {
   final communityRepository = ref.watch(communityRepositoryProvider);
@@ -28,18 +34,45 @@ final communityControllerProvider = StateNotifierProvider<CommunityController, b
     ref: ref,
   );
 });
+// A provider named `communityControllerProvider` that provides an instance of the `CommunityController` class.
+// It uses the `StateNotifierProvider` class to create the provider.
+// The `CommunityController` class requires two dependencies: `communityRepository` and `storageRepository`, which are obtained by accessing the respective providers using `ref.watch`.
+// The `CommunityController` instance is created with the obtained dependencies and the `ref` parameter.
+// This provider is responsible for managing the state and handling the logic related to community operations in the social network app.
+// Usage: `final communityController = useProvider(communityControllerProvider);`
 
 final getCommunityByNameProvider = StreamProvider.family((ref, String name) {
   return ref.watch(communityControllerProvider.notifier).getCommunityByName(name);
 });
+// A provider named `getCommunityByNameProvider` that exposes a stream of a community based on its name.
+// It uses the `StreamProvider.family` constructor to provide a family of providers.
+// The `ref` parameter and a `String` parameter `name` are passed to the provider.
+// The stream is obtained by calling the `getCommunityByName` method on the `communityControllerProvider` notifier.
+// The `communityControllerProvider` is accessed using the `ref.watch` function.
+// This provider allows dynamically fetching a community by its name and provides the community data as a stream to the consumer widgets.
+// Usage: `final communityStream = useProvider(getCommunityByNameProvider('communityName'));`
 
 final searchCommunityProvider = StreamProvider.family((ref, String query) {
   return ref.watch(communityControllerProvider.notifier).searchCommunity(query);
 });
+// A provider named `searchCommunityProvider` that exposes a stream of search results for a given query.
+// It uses the `StreamProvider.family` constructor to provide a family of providers.
+// The `ref` parameter and a `String` parameter `query` are passed to the provider.
+// The stream is obtained by calling the `searchCommunity` method on the `communityControllerProvider` notifier.
+// The `communityControllerProvider` is accessed using the `ref.watch` function.
+// This provider allows dynamically searching for communities based on a query and provides the search results as a stream to the consumer widgets.
+// Usage: `final searchResults = useProvider(searchCommunityProvider('searchQuery'));`
 
 final getCommunityPostsProvider = StreamProvider.family((ref, String name) {
   return ref.read(communityControllerProvider.notifier).getCommunityPosts(name);
 });
+// A provider named `getCommunityPostsProvider` that exposes a stream of posts for a specific community.
+// It uses the `StreamProvider.family` constructor to provide a family of providers.
+// The `ref` parameter and a `String` parameter `name` are passed to the provider.
+// The stream is obtained by calling the `getCommunityPosts` method on the `communityControllerProvider` notifier.
+// The `communityControllerProvider` is accessed using the `ref.read` function.
+// This provider allows dynamically fetching posts for a specific community and provides the post data as a stream to the consumer widgets.
+// Usage: `final communityPostsStream = useProvider(getCommunityPostsProvider('communityName'));`
 
 class CommunityController extends StateNotifier<bool> {
   final CommunityRepository _communityRepository;
@@ -72,6 +105,22 @@ class CommunityController extends StateNotifier<bool> {
       Routemaster.of(context).pop();
     });
   }
+  // The `createCommunity` method in the `CommunityController` class.
+  // It takes two parameters: `name` of type `String` representing the name of the community, and `context` of type `BuildContext`.
+  // It is responsible for creating a new community with the given name.
+
+  // The method sets the state to `true` to indicate that the creation process has started.
+  // It obtains the user's ID from the `userProvider` using `_ref.read(userProvider)?.uid`.
+  // Then it creates a new `Community` object with the provided `name`, default `banner` and `avatar`, and sets the `members` and `mods` to contain the user's ID.
+
+  // After creating the `community`, it calls the `createCommunity` method on the `_communityRepository` to create the community.
+  // The state is set to `false` to indicate that the creation process is completed.
+
+  // The method handles the result using the `fold` method. If the result is a `left` value (Failure), it shows a snackbar with the error message using the `showSnackBar` method.
+  // If the result is a `right` value (Success), it shows a snackbar with a success message and navigates back using `Routemaster.of(context).pop()`.
+
+  // This method allows the creation of a new community and provides appropriate feedback using snackbar messages.
+  // Usage: `communityController.createCommunity('communityName', context);`
 
   void joinCommunity(Community community, BuildContext context) async {
     final user = _ref.read(userProvider)!;
@@ -91,15 +140,42 @@ class CommunityController extends StateNotifier<bool> {
       }
     });
   }
+  // The `joinCommunity` method in the `CommunityController` class.
+  // It takes two parameters: `community` of type `Community` and `context` of type `BuildContext`.
+  // It is responsible for allowing a user to join or leave a community.
+
+  // It first obtains the current user from the `userProvider` using `_ref.read(userProvider)`.
+  // Then it declares a variable `res` of type `Either<Failure, void>` to store the result of the operation.
+
+  // If the `community` already contains the user in its `members` list, it calls the `leaveCommunity` method on the `_communityRepository` to leave the community.
+  // Otherwise, it calls the `joinCommunity` method on the `_communityRepository` to join the community.
+
+  // The method then handles the result using the `fold` method. If the result is a `left` value (Failure), it shows a snackbar with the error message using the `showSnackBar` method.
+  // If the result is a `right` value (Success), it shows a snackbar with the appropriate message based on whether the user joined or left the community.
+
+  // This method allows a user to join or leave a community and provides appropriate feedback using snackbar messages.
+  // Usage: `communityController.joinCommunity(community, context);`
 
   Stream<List<Community>> getUserCommunities() {
     final uid = _ref.read(userProvider)!.uid;
     return _communityRepository.getUserCommunities(uid);
   }
+  // The `getCommunityByName` method in the `_communityRepository` class.
+  // It takes a `String` parameter `name` representing the name of the community.
+  // It returns a stream of a single `Community` object.
+  // The stream is obtained by calling the `getCommunityByName` method on the `_communityRepository`.
+  // This method retrieves a specific community by its name from the community repository.
+  // Usage: `final communityStream = communityRepository.getCommunityByName('communityName');`
 
   Stream<Community> getCommunityByName(String name) {
     return _communityRepository.getCommunityByName(name);
   }
+  // The `getCommunityByName` method in the `CommunityController` class.
+  // It takes a `String` parameter `name` representing the name of the community.
+  // It returns a stream of a single `Community` object.
+  // The stream is obtained by calling the `getCommunityByName` method on the `_communityRepository`.
+  // This method retrieves a specific community by its name from the community repository.
+  // Usage: `final communityStream = communityController.getCommunityByName('communityName');`
 
   void editCommunity({
     required File? profileFile,
@@ -145,10 +221,33 @@ class CommunityController extends StateNotifier<bool> {
           (r) => Routemaster.of(context).pop(),
     );
   }
+  // The `editCommunity` method in the `CommunityController` class.
+  // It takes several parameters including `profileFile`, `bannerFile`, `profileWebFile`, `bannerWebFile`, `context`, and `community`.
+  // It is responsible for editing a community by updating its profile and banner images and other details.
+
+  // The method sets the state to `true` to indicate that the editing process has started.
+  // If `profileFile` or `profileWebFile` is provided, it stores the file in the storage repository using the `_storageRepository.storeFile` method.
+  // If successful, it updates the `community` object with the new avatar (profile image).
+  // If `bannerFile` or `bannerWebFile` is provided, it stores the file in the storage repository for the community's banner image.
+  // If successful, it updates the `community` object with the new banner image.
+
+  // After updating the profile and banner images, it calls the `editCommunity` method on the `_communityRepository` to save the changes to the community details.
+  // The state is set to `false` to indicate that the editing process is completed.
+  // If successful, it navigates back using `Routemaster.of(context).pop()`.
+  // If any errors occur during the process, it shows a snackbar with the error message using the `showSnackBar` method.
+
+  // This method handles the editing of a community, including updating its profile and banner images and other details.
+  // Usage: `communityController.editCommunity(profileFile: file, context: context, community: community);`
 
   Stream<List<Community>> searchCommunity(String query) {
     return _communityRepository.searchCommunity(query);
   }
+  // The `searchCommunity` method in the `CommunityController` class.
+  // It takes a `String` parameter `query` representing the search query.
+  // It returns a stream of a list of `Community` objects.
+  // The stream is obtained by calling the `searchCommunity` method on the `_communityRepository`.
+  // This method performs a search for communities based on the provided query in the community repository.
+  // Usage: `final searchResultsStream = communityController.searchCommunity('searchQuery');`
 
   void addMods(String communityName, List<String> uids, BuildContext context) async {
     final res = await _communityRepository.addMods(communityName, uids);
@@ -157,8 +256,24 @@ class CommunityController extends StateNotifier<bool> {
           (r) => Routemaster.of(context).pop(),
     );
   }
+  // The `addMods` method in the `CommunityController` class.
+  // It takes three parameters: `communityName` (String) representing the name of the community, `uids` (List<String>) representing the user IDs of the moderators to be added, and `context` (BuildContext) for showing a snackbar and navigating.
+
+  // The method calls the `addMods` method on the `_communityRepository`, passing the `communityName` and `uids`.
+  // It awaits the result and handles it using the `fold` method.
+  // If the result is a left value (Failure), it shows a snackbar with the error message using the `showSnackBar` method.
+  // If the result is a right value (Success), it navigates back using `Routemaster.of(context).pop()`.
+
+  // This method adds moderators to a community in the social network app and handles success or failure scenarios.
+  // Usage: `communityController.addMods('communityName', ['uid1', 'uid2'], context);`
 
   Stream<List<Post>> getCommunityPosts(String name) {
     return _communityRepository.getCommunityPosts(name);
   }
+  // The `getCommunityPosts` method in the `CommunityController` class.
+  // It takes a `String` parameter `name` representing the name of the community.
+  // It returns a stream of a list of `Post` objects.
+  // The stream is obtained by calling the `getCommunityPosts` method on the `_communityRepository`.
+  // This method retrieves the posts for a specific community from the community repository.
+  // Usage: `final postsStream = communityController.getCommunityPosts('communityName');`
 }
--- lib/features/community/repository/communitory_repository.dart
+++ lib/features/community/repository/communitory_repository.dart
@@ -11,6 +11,14 @@ import 'package:jana_soz/models/post_model.dart';
 final communityRepositoryProvider = Provider((ref) {
   return CommunityRepository(firestore: ref.watch(firestoreProvider));
 });
+// A provider named `communityRepositoryProvider` that provides an instance of the `CommunityRepository` class.
+// It uses the `Provider` constructor to define the provider.
+// The provider takes a function that receives a `ref` parameter.
+// Inside the function, it creates and returns a new instance of `CommunityRepository` by passing the `firestoreProvider` to the constructor.
+// The `firestoreProvider` is accessed using the `ref.watch` function.
+
+// This provider is responsible for providing the `CommunityRepository` instance that interacts with the Firestore database to perform community-related operations.
+// Usage: `final communityRepository = useProvider(communityRepositoryProvider);`
 
 class CommunityRepository {
   final FirebaseFirestore _firestore;
@@ -30,6 +38,21 @@ class CommunityRepository {
       return left(Failure(e.toString()));
     }
   }
+  // The `createCommunity` method in the `CommunityRepository` class.
+  // It takes a `Community` object as a parameter.
+  // It returns a `FutureEither<void>` which represents the success or failure of the operation.
+
+  // The method first checks if a community with the same name already exists by querying the Firestore document using `_communities.doc(community.name).get()`.
+  // If the document exists, it throws an error indicating that a community with the same name already exists.
+
+  // If the document doesn't exist, it proceeds to create the community by calling `_communities.doc(community.name).set(community.toMap())`.
+  // It uses the `toMap()` method of the `Community` object to convert it to a map representation.
+
+  // If any Firebase-related exceptions occur, it throws the corresponding error message.
+  // If any other exceptions occur, it returns a `left` value (Failure) with the error message.
+
+  // This method creates a new community in the Firestore database and handles success and error scenarios.
+  // Usage: `communityRepository.createCommunity(community);`
 
   FutureVoid joinCommunity(String communityName, String userId) async {
     try {
@@ -42,6 +65,18 @@ class CommunityRepository {
       return left(Failure(e.toString()));
     }
   }
+  // The `joinCommunity` method in the `CommunityRepository` class.
+  // It takes two parameters: `communityName` of type `String` representing the name of the community, and `userId` of type `String` representing the ID of the user.
+  // It returns a `FutureEither<void>` which represents the success or failure of the operation.
+
+  // The method updates the document of the specified community by calling `_communities.doc(communityName).update()` with the field and value to be updated.
+  // It uses the `FieldValue.arrayUnion` method to add the `userId` to the `members` array field of the document.
+
+  // If any Firebase-related exceptions occur, it throws the corresponding error message.
+  // If any other exceptions occur, it returns a `left` value (Failure) with the error message.
+
+  // This method allows a user to join a community by updating the `members` array field in the Firestore database and handles success and error scenarios.
+  // Usage: `communityRepository.joinCommunity('communityName', 'userId');`
 
   FutureVoid leaveCommunity(String communityName, String userId) async {
     try {
@@ -54,6 +89,18 @@ class CommunityRepository {
       return left(Failure(e.toString()));
     }
   }
+  // The `leaveCommunity` method in the `CommunityRepository` class.
+  // It takes two parameters: `communityName` of type `String` representing the name of the community, and `userId` of type `String` representing the ID of the user.
+  // It returns a `FutureEither<void>` which represents the success or failure of the operation.
+
+  // The method updates the document of the specified community by calling `_communities.doc(communityName).update()` with the field and value to be updated.
+  // It uses the `FieldValue.arrayRemove` method to remove the `userId` from the `members` array field of the document.
+
+  // If any Firebase-related exceptions occur, it throws the corresponding error message.
+  // If any other exceptions occur, it returns a `left` value (Failure) with the error message.
+
+  // This method allows a user to leave a community by updating the `members` array field in the Firestore database and handles success and error scenarios.
+  // Usage: `communityRepository.leaveCommunity('communityName', 'userId');`
 
   Stream<List<Community>> getUserCommunities(String uid) {
     return _communities.where('members', arrayContains: uid).snapshots().map((event) {
@@ -64,10 +111,38 @@ class CommunityRepository {
       return communities;
     });
   }
+  // The `getUserCommunities` method in the `CommunityRepository` class.
+  // It takes a `uid` parameter of type `String`, representing the ID of the user.
+  // It returns a stream of a list of `Community` objects.
+
+  // The method retrieves communities from the Firestore collection `_communities` where the `members` array field contains the specified `uid`.
+  // It uses the `snapshots()` method to listen for real-time updates to the collection.
+
+  // Inside the `map` callback function, it iterates over the `event.docs` and converts each document data to a `Community` object using the `Community.fromMap` method.
+  // The `fromMap` method takes a `Map<String, dynamic>` as input and constructs a `Community` object.
+
+  // The method returns a stream that emits a list of `Community` objects whenever there are updates to the collection.
+
+  // This method retrieves and listens to communities that the user is a member of from the Firestore database and converts them to `Community` objects.
+  // Usage: `communityRepository.getUserCommunities('userId');`
 
   Stream<Community> getCommunityByName(String name) {
     return _communities.doc(name).snapshots().map((event) => Community.fromMap(event.data() as Map<String, dynamic>));
   }
+  // The `getCommunityByName` method in the `CommunityRepository` class.
+  // It takes a `name` parameter of type `String`, representing the name of the community.
+  // It returns a stream of a single `Community` object.
+
+  // The method retrieves a specific community document from the Firestore collection `_communities` by calling `_communities.doc(name)`.
+  // It uses the `snapshots()` method to listen for real-time updates to the document.
+
+  // Inside the `map` callback function, it converts the document data to a `Community` object using the `Community.fromMap` method.
+  // The `fromMap` method takes a `Map<String, dynamic>` as input and constructs a `Community` object.
+
+  // The method returns a stream that emits a single `Community` object whenever there are updates to the community document.
+
+  // This method retrieves and listens to a specific community document from the Firestore database and converts it to a `Community` object.
+  // Usage: `communityRepository.getCommunityByName('communityName');`
 
   FutureVoid editCommunity(Community community) async {
     try {
@@ -78,6 +153,18 @@ class CommunityRepository {
       return left(Failure(e.toString()));
     }
   }
+  // The `editCommunity` method in the `CommunityRepository` class.
+  // It takes a `community` parameter of type `Community`, representing the community to be edited.
+  // It returns a `FutureEither<void>` which represents the success or failure of the operation.
+
+  // The method updates the document of the specified community by calling `_communities.doc(community.name).update()` with the updated data.
+  // It passes the `toMap()` method of the `community` object to update the document fields based on the provided data.
+
+  // If any Firebase-related exceptions occur, it throws the corresponding error message.
+  // If any other exceptions occur, it returns a `left` value (Failure) with the error message.
+
+  // This method edits a community by updating its document in the Firestore database and handles success and error scenarios.
+  // Usage: `communityRepository.editCommunity(community);`
 
   Stream<List<Community>> searchCommunity(String query) {
     return _communities
@@ -100,6 +187,23 @@ class CommunityRepository {
       return communities;
     });
   }
+  // The `searchCommunity` method in the `CommunityRepository` class.
+  // It takes a `query` parameter of type `String`, representing the search query.
+  // It returns a stream of a list of `Community` objects.
+
+  // The method performs a Firestore query on the `_communities` collection.
+  // It uses the `where` method to filter the documents based on the `name` field.
+  // If the `query` is empty, it retrieves all communities. Otherwise, it performs a range query using `isGreaterThanOrEqualTo` and `isLessThan` to match communities whose names are within a certain range.
+
+  // The `snapshots()` method is called to listen for real-time updates to the collection.
+
+  // Inside the `map` callback function, it iterates over the `event.docs` and converts each document data to a `Community` object using the `Community.fromMap` method.
+  // The `fromMap` method takes a `Map<String, dynamic>` as input and constructs a `Community` object.
+
+  // The method returns a stream that emits a list of `Community` objects whenever there are updates to the collection.
+
+  // This method allows searching for communities based on the name field in the Firestore database and converts the matching documents to `Community` objects.
+  // Usage: `communityRepository.searchCommunity('searchQuery');`
 
   FutureVoid addMods(String communityName, List<String> uids) async {
     try {
@@ -112,6 +216,18 @@ class CommunityRepository {
       return left(Failure(e.toString()));
     }
   }
+  // The `addMods` method in the `CommunityRepository` class.
+  // It takes two parameters: `communityName` of type `String` representing the name of the community, and `uids` of type `List<String>` representing the IDs of the moderators.
+  // It returns a `FutureEither<void>` which represents the success or failure of the operation.
+
+  // The method updates the document of the specified community by calling `_communities.doc(communityName).update()` with the field and value to be updated.
+  // It sets the `mods` field of the document to the provided list of moderator IDs.
+
+  // If any Firebase-related exceptions occur, it throws the corresponding error message.
+  // If any other exceptions occur, it returns a `left` value (Failure) with the error message.
+
+  // This method adds moderators to a community by updating the `mods` field in the Firestore database and handles success and error scenarios.
+  // Usage: `communityRepository.addMods('communityName', ['moderator1', 'moderator2']);`
 
   Stream<List<Post>> getCommunityPosts(String name) {
     return _posts.where('communityName', isEqualTo: name).orderBy('createdAt', descending: true).snapshots().map(
@@ -124,6 +240,14 @@ class CommunityRepository {
           .toList(),
     );
   }
+  // Inside the map callback function, it maps over the event.docs and converts each document data to a Post object using the Post.fromMap method.
+  // The fromMap method takes a Map<String, dynamic> as input and constructs a Post object.
+
+  // The method returns a stream that emits a list of Post objects whenever there are updates to the collection.
+
+  // This method retrieves and listens to posts associated with a specific community from the Firestore database and converts them to Post objects.
+  // The posts are ordered by their creation time in descending order.
+  // Usage: communityRepository.getCommunityPosts('communityName');
 
   CollectionReference get _posts => _firestore.collection(FirebaseConstants.postsCollection);
   CollectionReference get _communities => _firestore.collection(FirebaseConstants.communitiesCollection);
